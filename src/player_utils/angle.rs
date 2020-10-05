4872fa2e61338d25c6302d7da819438dfeaad3d0
Krzysztof Naglik <krzysiula1313@gmail.com>
2020-10-05 23:19:11 +0200

    Add rays to the Angle struct
    - rays (which is Vec of LinearGraph) member to the Angle struct added
    - additionally get_rays_angle_range function added which returns Range(s) that will be used to iterate over rays. (Range(s) bounded by start and end of the Angle)

--- src/player_utils/angle.rs
+++ src/player_utils/angle.rs
@@ -1,3 +1,10 @@
+use crate::graph::LinearGraph;
+use std::vec::Vec;
+
+struct Rays(Vec<LinearGraph>);
+
+const PI_2: f64 = std::f64::consts::PI * 2.0;
+
 // Angle start-end direction:
 // 1.reverse clocwise
 // 2.same as the coordinate system
@@ -17,11 +24,11 @@
 //       |
 #[derive(Debug)]
 pub struct Angle {
-    pub start: f64, // radians
-    pub end: f64,   // radians
+    pub start: f64,             // radians
+    pub end: f64,               // radians
+    rays: Vec<LinearGraph>,     // all rays around the player
+    radians_as_rays_index: f64, // number of rays devided by 2 PI
 }
-
-const PI_2: f64 = std::f64::consts::PI * 2.0;
 
 fn rotate(angle: &mut f64, value: f64) {
     if *angle + value < 0.0 {
@@ -34,6 +41,15 @@ fn rotate(angle: &mut f64, value: f64) {
 }
 
 impl Angle {
+    pub fn new(start: f64, end: f64, number_of_rays: usize) -> Angle {
+        Angle {
+            start: start,
+            end: end,
+            rays: LinearGraph::get_all_rays(number_of_rays),
+            radians_as_rays_index: number_of_rays as f64 / PI_2,
+        }
+    }
+
     pub fn value(&self) -> f64 {
         if self.start < self.end {
             return self.end - self.start;
@@ -46,6 +62,33 @@ impl Angle {
         rotate(&mut self.start, angle_delta);
         rotate(&mut self.end, angle_delta);
     }
+
+    pub fn get_rays_angle_range(&self) -> std::vec::Vec<std::ops::Range<usize>> {
+        if self.start > self.end {
+            return vec![
+                std::ops::Range {
+                    start: self.start_into_rays_index(),
+                    end: self.rays.len() - 1,
+                },
+                std::ops::Range {
+                    start: 0,
+                    end: self.end_into_rays_index(),
+                },
+            ];
+        }
+        vec![std::ops::Range {
+            start: self.start_into_rays_index(),
+            end: self.end_into_rays_index(),
+        }]
+    }
+
+    fn start_into_rays_index(&self) -> usize {
+        (self.radians_as_rays_index * self.start).floor() as usize
+    }
+
+    fn end_into_rays_index(&self) -> usize {
+        (self.radians_as_rays_index * self.end).ceil() as usize
+    }
 }
 
 #[cfg(test)]
@@ -56,14 +99,8 @@ mod test {
     fn angle_value() {
         let start_angle = 0.1;
         let end_angle = 0.6;
-        let angle_1 = Angle {
-            start: start_angle,
-            end: end_angle,
-        };
-        let angle_2 = Angle {
-            start: end_angle,
-            end: start_angle,
-        };
+        let angle_1 = Angle::new(start_angle, end_angle, 100);
+        let angle_2 = Angle::new(end_angle, start_angle, 100);
 
         assert_eq!(angle_1.value(), end_angle - start_angle);
         assert_ne!(angle_1.value(), PI_2 - end_angle + start_angle);
@@ -79,10 +116,7 @@ mod test {
 
         let mut start_angle = 3.2;
         let mut end_angle = 3.9;
-        let mut angle = Angle {
-            start: start_angle,
-            end: end_angle,
-        };
+        let mut angle = Angle::new(start_angle, end_angle, 100);
         assert_eq!(angle.start, start_angle);
         assert_eq!(angle.end, end_angle);
 
@@ -105,10 +139,7 @@ mod test {
 
         let mut start_angle = 5.1;
         let mut end_angle = 5.5;
-        let mut angle = Angle {
-            start: start_angle,
-            end: end_angle,
-        };
+        let mut angle = Angle::new(start_angle, end_angle, 100);
         assert_eq!(angle.start, start_angle);
         assert_eq!(angle.end, end_angle);
 
@@ -122,5 +153,45 @@ mod test {
         angle.rotate(negative_delta);
         assert_eq!(angle.start, start_angle + negative_delta);
         assert_eq!(angle.end, end_angle + negative_delta + PI_2);
+    }
+
+    #[test]
+    fn get_rays_angle_1_range() {
+        let start_angle = 5.1;
+        let end_angle = 5.5;
+        let number_of_rays = 100;
+        let angle = Angle::new(start_angle, end_angle, number_of_rays);
+        let ranges = angle.get_rays_angle_range();
+
+        assert_eq!(ranges.len(), 1);
+        assert_eq!(
+            ranges[0].start,
+            (start_angle * number_of_rays as f64 / PI_2).floor() as usize
+        );
+        assert_eq!(
+            ranges[0].end,
+            (end_angle * number_of_rays as f64 / PI_2).ceil() as usize
+        );
+    }
+
+    #[test]
+    fn get_rays_angle_2_ranges() {
+        let start_angle = 5.1;
+        let end_angle = 0.5;
+        let number_of_rays = 100;
+        let angle = Angle::new(start_angle, end_angle, number_of_rays);
+        let ranges = angle.get_rays_angle_range();
+
+        assert_eq!(ranges.len(), 2);
+        assert_eq!(
+            ranges[0].start,
+            (start_angle * number_of_rays as f64 / PI_2).floor() as usize
+        );
+        assert_eq!(ranges[0].end, number_of_rays - 1);
+        assert_eq!(ranges[1].start, 0);
+        assert_eq!(
+            ranges[1].end,
+            (end_angle * number_of_rays as f64 / PI_2).ceil() as usize
+        );
     }
 }
