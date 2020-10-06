aabc4d758cc89e3a66db2d48ad58451709545005
krisukox <krzysiula1313@gmail.com>
2020-10-06 15:35:09 +0200

    Add ray cast method to the Map struct

--- src/graph/coordinate.rs
+++ src/graph/coordinate.rs
@@ -1,4 +1,4 @@
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct Coordinate {
     pub x: f64,
     pub y: f64,
@@ -9,6 +9,41 @@ impl Coordinate {
         return (((self.x - coordinate.x).powf(2_f64) + (self.y - coordinate.y).powf(2_f64))
             as f64)
             .sqrt();
+    }
+
+    pub fn get_nearest_coordinates(&self) -> Vec<Coordinate> {
+        let x_floor = self.x.floor();
+        let y_floor = self.y.floor();
+        if x_floor == self.x && y_floor == self.y {
+            return vec![self.clone()];
+        } else if x_floor == self.x {
+            return vec![
+                Coordinate {
+                    x: self.x,
+                    y: y_floor,
+                },
+                Coordinate {
+                    x: self.x,
+                    y: self.y.ceil(),
+                },
+            ];
+        } else if y_floor == self.y {
+            return vec![
+                Coordinate {
+                    x: x_floor,
+                    y: self.y,
+                },
+                Coordinate {
+                    x: self.x.ceil(),
+                    y: self.y,
+                },
+            ];
+        }
+        println!("It shouldn't heppend but treat it normally");
+        return vec![Coordinate {
+            x: self.x.round(),
+            y: self.y.round(),
+        }];
     }
 }
 
@@ -32,6 +67,56 @@ mod tests {
         assert_eq!(
             coordinate_3.distance(&coordinate_1),
             (9_f64 + 16_f64).sqrt()
+        );
+    }
+
+    #[test]
+    fn get_near_coordinates() {
+        let coordinate_1 = Coordinate { x: 1.0, y: 2.5 };
+        let coordinate_2 = Coordinate { x: 3.5, y: 4.0 };
+        let coordinate_3 = Coordinate { x: 5.0, y: 6.0 };
+        let coordinate_4 = Coordinate { x: 5.5, y: 4.5 };
+        let points_1 = coordinate_1.get_nearest_coordinates();
+        let points_2 = coordinate_2.get_nearest_coordinates();
+        let points_3 = coordinate_3.get_nearest_coordinates();
+        let points_4 = coordinate_4.get_nearest_coordinates();
+        assert_eq!(points_1.len(), 2);
+        assert_eq!(points_2.len(), 2);
+        assert_eq!(points_3.len(), 1);
+        assert_eq!(points_4.len(), 1);
+
+        assert!(points_1.contains(&Coordinate {
+            x: coordinate_1.x,
+            y: coordinate_1.y.ceil()
+        }));
+        assert!(points_1.contains(&Coordinate {
+            x: coordinate_1.x,
+            y: coordinate_1.y.floor()
+        }));
+
+        assert!(points_2.contains(&Coordinate {
+            x: coordinate_2.x.ceil(),
+            y: coordinate_2.y
+        }));
+        assert!(points_2.contains(&Coordinate {
+            x: coordinate_2.x.floor(),
+            y: coordinate_2.y
+        }));
+
+        assert_eq!(
+            points_3[0],
+            Coordinate {
+                x: coordinate_3.x,
+                y: coordinate_3.y
+            }
+        );
+
+        assert_eq!(
+            points_4[0],
+            Coordinate {
+                x: coordinate_4.x.round(),
+                y: coordinate_4.y.round()
+            }
         );
     }
 }
--- src/map.rs
+++ src/map.rs
@@ -31,6 +31,30 @@ impl Map {
         let pixel = self.0.get_pixel(coordinate.x as u32, coordinate.y as u32);
         return pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0;
     }
+
+    pub fn cast_ray(
+        &self,
+        position: &graph::Coordinate,
+        ray: &graph::LinearGraph,
+    ) -> Vec<graph::Coordinate> {
+        let mut black_points: Vec<graph::Coordinate> = Vec::new();
+
+        let mut last_position = position.clone();
+        let mut next_position: graph::Coordinate;
+        loop {
+            next_position = ray.get_next(&last_position);
+            let points = next_position.get_nearest_coordinates();
+            for point in points {
+                if self.is_black_pixel(&point) {
+                    black_points.push(point);
+                }
+            }
+            if !black_points.is_empty() {
+                return black_points;
+            }
+            last_position = next_position;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -120,6 +144,69 @@ mod tests {
             assert_eq!(err_str, ERROR_STRING_WRONG_FORMAT);
         } else {
             panic!("new_map_image_wrong_format test failed");
+        }
+    }
+
+    #[test]
+    fn cast_ray_1() {
+        if let Ok(map) = Map::new("test_resources/map.png") {
+            let expected_black_points = vec![
+                graph::Coordinate { x: 32.0, y: 28.0 },
+                graph::Coordinate { x: 32.0, y: 29.0 },
+                graph::Coordinate { x: 32.0, y: 30.0 },
+                graph::Coordinate { x: 31.0, y: 30.0 },
+                graph::Coordinate { x: 30.0, y: 30.0 },
+            ];
+
+            let start_position = graph::Coordinate { x: 31.0, y: 29.0 };
+            let rays = vec![
+                graph::LinearGraph::from_radians(std::f64::consts::PI * 2.0 - 0.5),
+                graph::LinearGraph::from_radians(std::f64::consts::PI / 4.0),
+                graph::LinearGraph::from_radians(std::f64::consts::PI / 2.0 + 0.5),
+            ];
+            let mut black_points: Vec<graph::Coordinate> = Vec::new();
+
+            for ray in rays {
+                black_points.append(&mut map.cast_ray(&start_position, &ray))
+            }
+
+            assert_eq!(expected_black_points.len(), black_points.len());
+            for expected_black_point in expected_black_points {
+                assert!(black_points.contains(&expected_black_point));
+            }
+        }
+    }
+
+    #[test]
+    fn cast_ray_2() {
+        if let Ok(map) = Map::new("test_resources/map.png") {
+            let start_position = graph::Coordinate { x: 31.0, y: 29.0 };
+
+            let expected_black_points_1 = vec![
+                graph::Coordinate { x: 32.0, y: 29.0 },
+                graph::Coordinate { x: 32.0, y: 30.0 },
+                graph::Coordinate { x: 31.0, y: 30.0 },
+            ];
+            let rays_1 = vec![
+                graph::LinearGraph::from_radians(std::f64::consts::PI / 4.0 + 0.01),
+                graph::LinearGraph::from_radians(std::f64::consts::PI / 4.0 - 0.01),
+            ];
+
+            let expected_black_point_2 = graph::Coordinate { x: 32.0, y: 30.0 };
+            let ray_2 = graph::LinearGraph::from_radians(std::f64::consts::PI / 4.0);
+
+            let mut black_points_1: Vec<graph::Coordinate> = Vec::new();
+            for ray in rays_1 {
+                black_points_1.append(&mut map.cast_ray(&start_position, &ray))
+            }
+            assert_eq!(black_points_1.len(), 4);
+            for expected_black_point in expected_black_points_1 {
+                assert!(black_points_1.contains(&expected_black_point));
+            }
+
+            let black_points_2 = map.cast_ray(&start_position, &ray_2);
+            assert_eq!(black_points_2.len(), 1);
+            assert_eq!(black_points_2[0], expected_black_point_2);
         }
     }
 }
